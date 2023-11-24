0a2fbd30a908ed7cba6ad7a30a468a35a448dff0
YaminMyintNgwe <yaminmyintngwe@gmail.com>
2023-11-24 22:26:41 +0630

    -Validation Messages - Delete Confirm Dialog - Toast Message after deleted

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/model/user.model';
 import { UserService } from 'src/app/service/user.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit, OnDestroy {
   users : User[];
   id : number;
   addUser = false;
+  subscription : Subscription;
+  userId : number;
+  //isModalVisible: boolean = true;
+  //@ViewChild('liveToast') toast : ElementRef;
+  showToast = false;
 
   constructor(private userService : UserService, private route : ActivatedRoute, private router : Router) {}
 
@@ -20,22 +27,37 @@ export class UserListComponent implements OnInit {
   }
 
 
+  retrieveAllUsers() {
+    this.subscription = this.userService.getAllUsers().subscribe(
+      (users :  User[]) => {
+        this.users = users;
+        console.log("User List",this.users)
+      } 
+    );
+  }
+
+  openDeleteModal(id : number) {
+    this.userId = id;
+    console.log(this.userId);
+  }
+
   onDeleteUser(id : number) {
     console.log(id);
-    this.userService.deleteUser(id).subscribe(
+    this.userService.deleteUser(this.userId).subscribe(
       () => {
         // this.users = this.users.filter(item => item.id !== id);
         this.retrieveAllUsers();
       }
     )
+    //this.isModalVisible = false;
+    //$((<HTMLDivElement> this.toast.nativeElement).id).show();
+    this.showToast = true;
+    setTimeout(() => {
+      this.showToast = false;
+    }, 2000)
   }
 
-  retrieveAllUsers() {
-    this.userService.getAllUsers().subscribe(
-      (users : User[]) => {
-        this.users = users;
-        console.log("User List",this.users)
-      } 
-    );
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
